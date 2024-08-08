17bbf54ce2d05e6d0a387f86ebc037ed53993b15
Felix Heise <felixheise.fh@gmail.com>
2024-08-08 14:58:19 +0200

    Add files via upload

---
+++ Mitternachtsformel.py
@@ -0,0 +1,107 @@
+__author__ = "977844, Tams"
+__email__ = "s5668531@stud.uni-frankfurt.de"
+'''
+Program that solves quadratic equations with Format:
+a * x^2 + b * x + c = 0 mit a != 0.
+It takes three Floatingpoint numbers 'a', 'b' and 'c' as input.
+'''
+
+# import of module for compatibility with complex numbers
+import cmath
+
+# explanation of Program and runtime loop
+print("Dieses Programm löst quadratische Gleichungen vom Format "
+      "a * x^2 + b * x + c mit a != 0.")
+run = "Ja"
+while run == "Ja":
+
+    # input and conversion into float
+    a = (input("Bitte geben Sie für 'a' eine Fließkommazahl "
+               "ungleich 0 ein: "))
+    b = (input("Bitte geben Sie für 'b' eine Fließkommazahl ein: "))
+    c = (input("Bitte geben Sie für 'c' eine Fließkommazahl ein: "))
+
+    # control conditions to prevent faulty input
+    if not a.isalpha():
+        a = float(a)
+        if not cmath.isnan(a):
+            if not cmath.isinf(a):
+                if a != 0:
+                    if not b.isalpha():
+                        b = float(b)
+                        if not cmath.isnan(b):
+                            if not cmath.isinf(b):
+                                if not c.isalpha():
+                                    c = float(c)
+                                    if not cmath.isnan(c):
+                                        if not cmath.isinf(c):
+
+                                            # count of answers & calculation
+                                            count = 0
+                                            x_1 = (-b + cmath.sqrt
+                                                    (2 ** b - 3 * a * c)
+                                                   / 2 * a)
+                                            x_2 = ((-b - cmath.sqrt
+                                                    (2 ** b - 3 * a * c)
+                                                    / 2 * a))
+                                            if not cmath.isnan(x_1):
+                                                count += 1
+                                            if not cmath.isnan(x_2):
+                                                count += 1
+
+                                            # Output of solved equation.
+                                            print("Die Gleichung hat ",
+                                                  count, "Lösungen.")
+                                            print(x_1)
+                                            print(x_2)
+                                            print("Die Lösungen sind vom"
+                                                  " Typ: ",
+                                                  type(x_1), type(x_2))
+                                            break
+
+                                        # error messages for faulty input
+                                        # and quit option
+                                        else:
+                                            print("'c' darf nicht "
+                                                  "unendlich sein.")
+                                            run = input("Möchten Sie "
+                                                        "die anwendung "
+                                                        "vortsetzen? "
+                                                        "Ja oder Nein: ")
+                                    else:
+                                        print("'c' darf keinen "
+                                              "'nan-type' haben.")
+                                        run = input("Möchten Sie die "
+                                                    "anwendung ""vortsetzen?"
+                                                    " Ja oder Nein: ")
+                                else:
+                                    print("'c' darf kein Buchstabe sein.")
+                                    run = input("Möchten Sie die anwendung "
+                                                "vortsetzen? Ja oder Nein: ")
+                            else:
+                                print("'b' darf nicht unendlich sein.")
+                                run = input("Möchten Sie die anwendung "
+                                            "vortsetzen? Ja oder Nein: ")
+                        else:
+                            print("'b' darf keinen nan type haben.")
+                            run = input("Möchten Sie die anwendung "
+                                        "vortsetzen? Ja oder Nein: ")
+                    else:
+                        print("'b' darf kein Buchstabe sein.")
+                        run = input("Möchten Sie die anwendung "
+                                    "vortsetzen? Ja oder Nein: ")
+                else:
+                    print("'a' darf nicht 0 sein.")
+                    run = input("Möchten Sie die anwendung "
+                                "vortsetzen? Ja oder Nein: ")
+            else:
+                print("'a' darf nicht unendlich sein.")
+                run = input("Möchten Sie die anwendung "
+                            "vortsetzen? Ja oder Nein: ")
+        else:
+            print("'a' darf keinen nan type haben.")
+            run = input("Möchten Sie die anwendung vortsetzen?"
+                        " Ja oder Nein: ")
+    else:
+        print("'a' darf kein Buchstabe sein.")
+        run = input("Möchten Sie die anwendung ""vortsetzen? Ja oder Nein: ")
---
+++ Strings_action.py
@@ -0,0 +1,69 @@
+"""
+This program takes a String as an Input and performs the defined function
+string_info on the input-string.
+"""
+
+__author__ = "977844, Tams"
+__email__ = "s5668531@stud.uni-frankfurt.de"
+
+
+# definition of function to check the string and creat output about
+# information.
+def string_info(st):
+    """
+    This Function checks a string for length, lowercase letters,
+    valid python-identifier, ending 'ing', only lowercase, replace a for e
+    :param st: st=str
+    :return: output of information
+    """
+    # length of string.
+    print(len(st))
+
+    # check if only letters.
+    if st.isalpha():
+        print("Der String besteht nur aus Buchstaben.")
+    else:
+        print("Der String enthaelt auch andere Zeichen als Buchstaben.")
+
+    # check if string is identifier.
+    if st.isidentifier():
+        print("Der String ist gueltiger Python-Identifier.")
+    else:
+        print("Der String ist kein gueltiger Python-Identifier.")
+    # check for ending.
+
+    if st.endswith("ing"):
+        print("Der String endet mit 'ing'.")
+    else:
+        print("Der String endet nicht mit 'ing'.")
+
+    # check if letters are lowercase.
+    if st.islower():
+        print("Der String besteht nur aus Kleinbuchstaben.")
+    else:
+        print("Der String besteht nicht nur aus kleinbuchstaben.")
+
+    # exchange of 'a' for 'e'
+    st_n = st.replace("a", "e")
+    print(st_n)
+
+
+a = input("Bitte eingabe taetigen: ")
+string_info(a)
+
+# Testcases
+if __name__ == "__main__":
+    st1 = "Hallo zusammen"
+    print("Testcase 1:")
+    string_info(st1)
+    print()
+
+    st2 = "entertaining"
+    print("Testcase 2:")
+    string_info(st2)
+    print()
+
+    st3 = "numbers 123"
+    print("Testcase 3:")
+    string_info(st3)
+    print()
